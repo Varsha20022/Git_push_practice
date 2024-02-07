53a9483beb02871310635b3974130f8f57ea889a
Varsha V <Varsha V@TYSS-Varsha-V>
2024-02-07 17:16:14 +0530

    First commit

---
+++ Github_practice/src/main/java/generic_library/Varsha.java
@@ -0,0 +1,20 @@
+package generic_library;
+
+public class Varsha 
+{
+	String name;
+	String emp_id;
+	long mobile_number;
+public void speak()
+{
+	System.out.println("I can speak");
+}
+public void reading()
+{
+	System.out.println("I love reading");
+}
+public void eating()
+{
+	System.out.println("Eating...");
+}
+}
